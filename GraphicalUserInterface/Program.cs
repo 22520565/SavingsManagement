7fa07b109b6411f4b6cd2d7957f2c5567d327e2d
22520565 <22520565@gm.uit.edu.vn>
2024-04-25 20:22:46 +0700

    Enhance main() function

--- GraphicalUserInterface/Program.cs
+++ GraphicalUserInterface/Program.cs
@@ -1,21 +1,37 @@
-namespace Gui
-{
-    using GraphicalUserInterface;
-    using System;
-    using System.Windows.Forms;
+namespace Gui;
 
-    internal static class Program
+using System;
+using System.Windows.Forms;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+
+        ApplicationConfiguration.Initialize();
+        using var loginForm = new LoginForm();
+        using var mainMenuForm = new MainMenuForm();
+        do
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form_Login());
-        }
+            Application.Run(loginForm);
+            if (!loginForm.UserSuccessfullyAuthenticated)
+            {
+                break;
+            }
+
+            Application.Run(mainMenuForm);
+            if (!mainMenuForm.GoingBackToLoginForm)
+            {
+                break;
+            }
+
+
+        } while (true);
     }
 }
