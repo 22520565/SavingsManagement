a96d20a4c90c6231b4b46cd893a487205fac8e07
22520565 <22520565@gm.uit.edu.vn>
2024-06-06 17:48:16 +0700

    Add Saving Report Form

---
+++ GraphicalUserInterface/SavingReportForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace GraphicalUserInterface
+{
+    partial class SavingReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportViewer.Location = new System.Drawing.Point(0, 0);
+            reportViewer.Name = "reportViewer";
+            reportViewer.ServerReport.BearerToken = null;
+            reportViewer.Size = new System.Drawing.Size(800, 450);
+            reportViewer.TabIndex = 0;
+            reportViewer.Load += reportViewer_Load;
+            // 
+            // SavingReportForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(reportViewer);
+            Name = "SavingReportForm";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+    }
+}
