0569c0b83695dafa09c2612fb1d58eb8db2a8e01
TruongDacDien <22520248@gm.uit.edu.vn>
2024-06-07 13:21:10 +0700

    Login/SigUp/ResetPassword

---
+++ GraphicalUserInterface/ForgotPasswordForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace GraphicalUserInterface
+{
+    partial class ForgotPasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            getOTPBtn = new System.Windows.Forms.Button();
+            confirmOTPBtn = new System.Windows.Forms.Button();
+            panel5 = new System.Windows.Forms.Panel();
+            pictureBox5 = new System.Windows.Forms.PictureBox();
+            label7 = new System.Windows.Forms.Label();
+            txtOTP = new System.Windows.Forms.TextBox();
+            panel4 = new System.Windows.Forms.Panel();
+            pictureBox4 = new System.Windows.Forms.PictureBox();
+            label3 = new System.Windows.Forms.Label();
+            txtEmail = new System.Windows.Forms.TextBox();
+            btnResetPassword = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.How_to_Password_Protect_Antivirus_Settings__Kaspersky_Daily;
+            pictureBox1.Location = new System.Drawing.Point(150, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(640, 480);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // getOTPBtn
+            // 
+            getOTPBtn.AutoSize = true;
+            getOTPBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            getOTPBtn.Location = new System.Drawing.Point(565, 610);
+            getOTPBtn.Name = "getOTPBtn";
+            getOTPBtn.Size = new System.Drawing.Size(160, 42);
+            getOTPBtn.TabIndex = 46;
+            getOTPBtn.Text = "Get OTP";
+            getOTPBtn.UseVisualStyleBackColor = true;
+            getOTPBtn.Click += getOTPBtn_Click;
+            // 
+            // confirmOTPBtn
+            // 
+            confirmOTPBtn.AutoSize = true;
+            confirmOTPBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            confirmOTPBtn.Location = new System.Drawing.Point(565, 673);
+            confirmOTPBtn.Name = "confirmOTPBtn";
+            confirmOTPBtn.Size = new System.Drawing.Size(160, 42);
+            confirmOTPBtn.TabIndex = 45;
+            confirmOTPBtn.Text = "Confirm OTP";
+            confirmOTPBtn.UseVisualStyleBackColor = true;
+            confirmOTPBtn.Click += confirmOTPBtn_Click;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(pictureBox5);
+            panel5.Controls.Add(label7);
+            panel5.Controls.Add(txtOTP);
+            panel5.Location = new System.Drawing.Point(204, 611);
+            panel5.Margin = new System.Windows.Forms.Padding(4);
+            panel5.Name = "panel5";
+            panel5.Size = new System.Drawing.Size(354, 104);
+            panel5.TabIndex = 44;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.security_shield__1_1;
+            pictureBox5.Location = new System.Drawing.Point(16, 10);
+            pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new System.Drawing.Size(30, 30);
+            pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            pictureBox5.TabIndex = 29;
+            pictureBox5.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label7.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            label7.Location = new System.Drawing.Point(52, 10);
+            label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(56, 28);
+            label7.TabIndex = 26;
+            label7.Text = "OTP";
+            // 
+            // txtOTP
+            // 
+            txtOTP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            txtOTP.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            txtOTP.Location = new System.Drawing.Point(16, 45);
+            txtOTP.Margin = new System.Windows.Forms.Padding(2);
+            txtOTP.Name = "txtOTP";
+            txtOTP.Size = new System.Drawing.Size(322, 40);
+            txtOTP.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(pictureBox4);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(txtEmail);
+            panel4.Location = new System.Drawing.Point(204, 499);
+            panel4.Margin = new System.Windows.Forms.Padding(4);
+            panel4.Name = "panel4";
+            panel4.Size = new System.Drawing.Size(521, 104);
+            panel4.TabIndex = 43;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.email;
+            pictureBox4.Location = new System.Drawing.Point(16, 14);
+            pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new System.Drawing.Size(16, 16);
+            pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            pictureBox4.TabIndex = 29;
+            pictureBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            label3.Location = new System.Drawing.Point(52, 10);
+            label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(72, 28);
+            label3.TabIndex = 26;
+            label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            txtEmail.Font = new System.Drawing.Font("Cambria", 13.8F);
+            txtEmail.Location = new System.Drawing.Point(16, 45);
+            txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new System.Drawing.Size(483, 40);
+            txtEmail.TabIndex = 9;
+            // 
+            // btnResetPassword
+            // 
+            btnResetPassword.AutoSize = true;
+            btnResetPassword.BackColor = System.Drawing.Color.Black;
+            btnResetPassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            btnResetPassword.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            btnResetPassword.ForeColor = System.Drawing.Color.White;
+            btnResetPassword.Location = new System.Drawing.Point(318, 753);
+            btnResetPassword.Margin = new System.Windows.Forms.Padding(2);
+            btnResetPassword.Name = "btnResetPassword";
+            btnResetPassword.Size = new System.Drawing.Size(308, 66);
+            btnResetPassword.TabIndex = 47;
+            btnResetPassword.Text = "Reset Password ";
+            btnResetPassword.UseVisualStyleBackColor = false;
+            btnResetPassword.Click += btnResetPassword_Click;
+            btnResetPassword.MouseEnter += btnResetPassword_MouseEnter;
+            btnResetPassword.MouseLeave += btnResetPassword_MouseLeave;
+            // 
+            // ForgotPasswordForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            ClientSize = new System.Drawing.Size(929, 860);
+            Controls.Add(btnResetPassword);
+            Controls.Add(getOTPBtn);
+            Controls.Add(confirmOTPBtn);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(pictureBox1);
+            Name = "ForgotPasswordForm";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "ForgotPasswordForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button getOTPBtn;
+        private System.Windows.Forms.Button confirmOTPBtn;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtOTP;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnResetPassword;
+    }
+}
