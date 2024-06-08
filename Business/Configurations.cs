5792790f365519a56142065b0db6eadc6dbde6ba
22520565 <22520565@gm.uit.edu.vn>
2024-06-08 10:45:24 +0700

    Add Configuration on data access and on business

---
+++ Business/Configurations.cs
@@ -0,0 +1,61 @@
+﻿namespace Business;
+
+using System.Linq;
+using DataAccess;
+
+public static class Configurations
+{
+    public static decimal MaxAmountOpeningSaving
+    {
+        get
+        {
+            using var context = new SavingsManagementContext();
+            return context.Configurations.FirstOrDefault()?.MaxAmountOpeningSaving ?? decimal.Zero;
+        }
+    }
+
+    public static decimal MinAmountOpeningSaving
+    {
+        get
+        {
+            using var context = new SavingsManagementContext();
+            return context.Configurations.FirstOrDefault()?.MinAmountOpeningSaving ?? decimal.Zero;
+        }
+    }
+
+    public static decimal MaxAmountDepositing
+    {
+        get
+        {
+            using var context = new SavingsManagementContext();
+            return context.Configurations.FirstOrDefault()?.MaxAmountDepositing ?? decimal.Zero;
+        }
+    }
+
+    public static decimal MinAmountDepositing
+    {
+        get
+        {
+            using var context = new SavingsManagementContext();
+            return context.Configurations.FirstOrDefault()?.MinAmountDepositing ?? decimal.Zero;
+        }
+    }
+
+    public static decimal MaxAmountWithdrawing
+    {
+        get
+        {
+            using var context = new SavingsManagementContext();
+            return context.Configurations.FirstOrDefault()?.MaxAmountWithdrawing ?? decimal.Zero;
+        }
+    }
+
+    public static decimal MinAmountWithdrawing
+    {
+        get
+        {
+            using var context = new SavingsManagementContext();
+            return context.Configurations.FirstOrDefault()?.MinAmountWithdrawing ?? decimal.Zero;
+        }
+    }
+}
---
+++ DataAccess/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+[Table("Configuration")]
+public sealed class Configuration
+{
+    [Key]
+    public byte ID { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal MaxAmountOpeningSaving { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal MinAmountOpeningSaving { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal MaxAmountDepositing { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal MinAmountDepositing { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal MaxAmountWithdrawing { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal MinAmountWithdrawing { get; set; }
+}
